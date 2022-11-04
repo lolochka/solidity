1c1079902aa6930a9d5b3f243bca4addd517f333
Remix Workspace <>
2022-11-04 17:23:50 +0200

    added fallback and receive

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,34 +2,44 @@
 pragma solidity ^0.8.8;
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
 
     //to convert to usd without decimal
     //min amount of ETH is 10 / 1528
-    uint256 public minUSD = 10 * 1e18;
+    //constant variable is not allocate place in memory
+    uint256 public constant MINIMUM_USD = 10 * 1e18;
+    //21.415 gas - constant
+    //23,515 gas - non-constant
+
+    address public immutable i_owner;
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
-    address public owner;
 
     constructor() {
         //address who deployed this contract
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable{
         //1 eth in wei
         //value is comming in wei e18
-        require (msg.value.getConversionRate() >= minUSD, "I need more money");
+        require (msg.value.getConversionRate() >= MINIMUM_USD, "I need more money");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
     //Decorator
     modifier onlyOwner {
         //Check the owner of the contract
-        require(msg.sender == owner, "Sender is not owner. You are not allowed to do this");
+        //require(msg.sender == i_owner, "Sender is not owner. You are not allowed to do this");
+        
+        /*we can also use error instance to show user indtead allocate
+        memrory with string*/
+        if(msg.sender == i_owner) { revert NotOwner(); }
         _; ///where to run fmodified function code
     }
 
@@ -53,5 +63,13 @@ contract FundMe {
         //call
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");        
         require(callSuccess, "Call failed");
+    }
+
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
