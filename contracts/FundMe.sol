6ac205bd25bd3cecc5b09885f5403e868b7952a4
Remix Workspace <>
2022-11-04 14:46:34 +0200

    added withdraw

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -11,6 +11,12 @@ contract FundMe {
 
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+    address public owner;
+
+    constructor() {
+        //address who deployed this contract
+        owner = msg.sender;
+    }
 
     function fund() public payable{
         //1 eth in wei
@@ -18,5 +24,34 @@ contract FundMe {
         require (msg.value.getConversionRate() >= minUSD, "I need more money");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
+    }
+
+    //Decorator
+    modifier onlyOwner {
+        //Check the owner of the contract
+        require(msg.sender == owner, "Sender is not owner. You are not allowed to do this");
+        _; ///where to run fmodified function code
+    }
+
+    function withdraw() public onlyOwner{
+        /* starting index, ending index, step count */
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        //reset the array
+        funders = new address[](0);
+
+        // //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        //call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");        
+        require(callSuccess, "Call failed");
     }
 }
